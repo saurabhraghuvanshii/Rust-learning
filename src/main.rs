6888daf1e551c24b779ab0bd43099d7b141d581b
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-06-04 04:05:20 +0530

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,60 @@
-fn is_even(num: i32) -> bool {
-    if num % 2 == 0{
-        return true;
-    }
-    return false;
-}
+// fn is_even(num: i32) -> bool {
+//     if num % 2 == 0{
+//         return true;
+//     }
+//     return false;
+// }
 
-fn fib( num: u32) -> u32 {
-    if num <= 1 {
-        return num
-    }
-    return fib( num - 1 ) + fib( num - 2 ); 
-}
+// fn fib( num: u32) -> u32 {
+//     if num <= 1 {
+//         return num
+//     }
+//     return fib( num - 1 ) + fib( num - 2 ); 
+// }
 
-fn fib2 ( num: u32 ) -> u32 {
-    let mut  first = 0;
-    let mut second = 1;
+// fn fib2 ( num: u32 ) -> u32 {
+//     let mut  first = 0;
+//     let mut second = 1;
 
-    if num == 0 {
-        return first;
-    }
+//     if num == 0 {
+//         return first;
+//     }
      
-    if num == 1 {
-        return  second;
-    }
+//     if num == 1 {
+//         return  second;
+//     }
     
-    for _ in 0..(num-1) {
-        let temp = second;
-        second = second + first;
-        first = temp;
-    }
+//     for _ in 0..(num-1) {
+//         let temp = second;
+//         second = second + first;
+//         first = temp;
+//     }
 
-    return second;
-}
+//     return second;
+// }
 
-fn get_str_length(str: String) ->usize{
-    str.chars().count()
+// fn get_str_length(str: String) ->usize{
+//     str.chars().count()
+// }
+
+// fn main() {
+//     println!("{}", is_even(7));
+//     println!("{}",fib(5) );
+//     println!("{}", fib2(5));
+//     let name = String::from("saurabh");
+//     let len = get_str_length(name);
+//     println!("{}", len);
+// }
+
+struct User {
+    name: String,
+    age: i32
 }
 
 fn main() {
-    println!("{}", is_even(7));
-    println!("{}",fib(5) );
-    println!("{}", fib2(5));
-    let name = String::from("saurabh");
-    let len = get_str_length(name);
-    println!("{}", len);
+    let user = User {
+        name: String::from("saurabh"),
+        age: 33,
+    };
+    println!("{} {}", user.name, user.age);
 }
